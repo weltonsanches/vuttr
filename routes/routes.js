f9d6894650c4a11c0a236617b36e6f8f8f809be5
Welton Sanches <sanches.welton@gmail.com>
2019-06-20 20:14:11 -0300

    adding blueprint docs

--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,24 @@
+const path = require('path');
 
 const appRouter = (app) => {
 
   app
+    .get('/', (req, res) => {
+      const html = `<h1>The <abbr title="Very Useful Tools to Remember">VUTTR</abbr> API is running here.</h1>
+      <p>To see more about this API read the <a href="http://localhost:3000/docs">docs</a></p>`;
+
+      res.send(html);
+    })
+    .get('/docs', (req, res) => {
+      res.sendFile(path.resolve(`${__dirname}/../docs/index.html`));
+    })
 		.get('/tools', (req, res) => {
 			res.send({ response: 'Tools'});
     })
     .post('/tools', (req, res) => {
       res.status(201).send('Ok');
     })
-    .delete('/', function (req, res) {
+    .delete('/tools/:id', function (req, res) {
       res.status(200).send('Ok');
     });
 };
