14e7f2ceab8c923590b0ff35f94d104705291cac
Welton Sanches <sanches.welton@gmail.com>
2019-07-14 22:53:30 -0300

    starting tests

---
+++ tests/tools.js
@@ -0,0 +1,10 @@
+const { expect } = require('chai');
+const ToolsController = require('../controllers/ToolsController');
+
+describe('Tools Controller tests', () => {
+  it('should return 1 Obj', () => {});
+  it('should return the new ID', () => {});
+  it('should return 1 array', () => {});
+  it('should return 1 array filtering by tag', () => {});
+  it('should delete an item', () => {});
+});
